83969ceddc33ec17a1a8b756d73100e832b1529f
sky3005pal <102282034+sky3005pal@users.noreply.github.com>
2024-08-03 18:55:14 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let someNumber = 38
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+//***************************  *******************************************
+
+let str1 ="Akash"
+let str2 =" Pal"
+let str3 = str1 + str2
+console.log(str3)
+console.log(1+ 2 + "2");
+console.log("1"+ 2 + 2);
