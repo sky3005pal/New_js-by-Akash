644d29b7fb48a2fbf20debcf8766bda3bbe9f1ac
sky3005pal <102282034+sky3005pal@users.noreply.github.com>
2024-08-04 07:03:30 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Akash "
+const num =  1234567890 
+// console.log(name + num + " value")
+console.log (`Hello my name is ${name} and my num is ${num}`);
+
+
+const gameName = new String('Akash');
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+
